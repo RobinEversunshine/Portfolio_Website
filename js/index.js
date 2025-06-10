77aa13738f926d7629f1801b534b567aea0206af
RobinEversunshine <robinrainup@gmail.com>
2025-06-09 21:07:45 -0400

    scroll fix

--- js/index.js
+++ js/index.js
@@ -312,7 +312,7 @@ window.addEventListener("resize", checkWidth);
 const lenis = new Lenis({
     duration: 1.2, // 过渡时间（秒）
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // 缓动函数
-    smooth: true // 开启平滑滚动
+    // smooth: true // 开启平滑滚动
 });
 
 function raf(time) {
